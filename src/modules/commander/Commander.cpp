aa054c3c9203f6790f8b55dca8282206facb4e7b
Julian Oes <julian@oes.ch>
2021-04-15 16:05:12 +0200

    Fix style

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -973,6 +973,7 @@ Commander::handle_command(const vehicle_command_s &cmd)
 					} else {
 						if (cmd_from_manual_stick) {
 							arming_res = arm(arm_disarm_reason_t::RC_STICK, !forced);
+
 						} else {
 							arming_res = arm(arm_disarm_reason_t::COMMAND_INTERNAL, !forced);
 						}
@@ -985,6 +986,7 @@ Commander::handle_command(const vehicle_command_s &cmd)
 					} else {
 						if (cmd_from_manual_stick) {
 							arming_res = disarm(arm_disarm_reason_t::RC_STICK);
+
 						} else {
 							arming_res = disarm(arm_disarm_reason_t::COMMAND_INTERNAL);
 						}
